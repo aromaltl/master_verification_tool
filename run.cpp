1bb9f649945b1fe3e081f85e9b4fbbad5782ce4a
aromaltl <103577905+aromaltl@users.noreply.github.com>
2023-08-07 10:45:47 +0000

    run modified

--- run.cpp
+++ run.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 int main() {
    system("echo $(date) >> log.txt 2>&1");
-   system("python3 master_verification.py >> log.txt 2>&1");
+   //system("python3 master_verification.py >> log.txt 2>&1");
+   system("chmod +x ./run.sh && ./run.sh >> log.txt 2>&1");
+   
+
    return 0;
 }
