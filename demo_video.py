64c4faaf48596ffbdb1ebb171147e06e7f90b4a9
aromaltl <103577905+aromaltl@users.noreply.github.com>
2024-02-22 07:34:34 +0000

    Revert "Update demo_video.py added color bounding box based on assets"
    This reverts commit a3e29795148fcfbb3781f60d7726e49f1c51ffec.

--- demo_video.py
+++ demo_video.py
@@ -6,25 +6,13 @@ import glob
 import os
 
 assets_dic={}
-videos=list(glob.glob("/*.MP4"))
+videos=list(glob.glob("/run/user/1000/gvfs/afp-volume:host=Anton.local,user=ml_support,volume=MachineLearning/POC/Jubail Royal Commission/takeleap_jubail-royal-commission_2023-02-05_1225/Jubail Royal Commission/jub/*.MP4"))
 videos.sort()
 print(videos)
 counter={}
 cap=cv2.VideoCapture(videos[0])
 dirname = os.path.basename(videos[0])
-COLORS =[
-    '#700409',  # Dark Red
-    '#007010',  # Dark Green
-    '#100090',  # Dark Blue
-    '#907000',  # Olive
-    '#600060',  # Dark Purple
-    '#0A4050',  # Teal
-    '#B04000',  # Sienna
-    '#80B040',  # Dark Salmon
-    '#603330',  # Dark Brown
-    '#54A050'   # Dim Gray
-]
-assets_color={}
+
 w, h =  int(cap.get(3)), int(cap.get(4))
 cap.release()
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -42,9 +30,8 @@ def addBBox(im, frameNo,data):
             if items[0] not in assets_dic[ass]:
                 assets_dic[ass][items[0]]=counter[ass]
                 counter[ass]+=1
-            if ass not in assets_color:
-                assets_color[ass]=COLORS[len(assets_dic.keys())%len(COLORS)]  
-            draw_bounding_box(im, (items[1][0],items[1][1],items[2][0],items[2][1]), labels=[str(assets_dic[ass][items[0]]), ass.replace("_"," ")], font_scale = 0.8,color=assets_color[ass]) 
+            # print()
+            draw_bounding_box(im, (items[1][0],items[1][1],items[2][0],items[2][1]), labels=[str(assets_dic[ass][items[0]]), ass], color='green') 
     return im
 
 for x in videos:
