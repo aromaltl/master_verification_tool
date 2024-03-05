698ac1db906eb2e12e815be8afc33e0e2dbe85d5
aromaltl <103577905+aromaltl@users.noreply.github.com>
2024-03-05 17:50:06 +0530

    Update config.py added remarks and comment

--- config.py
+++ config.py
@@ -14,4 +14,17 @@ config = {
         "right":'@#EWEA',
         "left": 'F#CTCFC'
     }
+    "remarks":['Direction of a toponym in the form of an arrow', 
+               'Information_Board_Bus_Shelter_Ahead_M', 'Information_Board_Going_Straight_M', 
+               'Information_Board_Parking_Brown_M', 'Information_Board_School_Ahead_M', 'Information_Board_Street_Name_Blue_M', 
+               'Information_Board_Street_Name_Green_M', 'Information_Parking_for_Disable_M', 'Mandatory passage from the left side of the island', 
+               'Mandatory passage from the right or left side of the island', 'Mandatory passage from the right side of the island', 'Caution_Bidirectional_Traffic_M', 
+               'Caution_Board_Compulsory yielding of priority', 'Caution_Board_Crosswalk_Blue_M', 'Caution_Board_Crosswalk_Yellow_M', 'Caution_Board_Main_Road_M', 'Caution_Board_School_Ahead_M',
+               'Caution_Board_Trams_M', 'Caution_Speed_Bumps_M', 'Compulsory direction of movement to the LEFT_M', 'Compulsory direction of movement to the right', 
+               'Compulsory passage from the right or from the left side of the island', 'Compulsory passage only from the left side of the island', 
+               'Compulsory passage only from the right side of the island', 'compulsory_stop', 'Mandatory circular route', 
+               'Mandatory_Board_Going_Straight_M', 'Maximum speed limit', 'No entry for all vehicles', 
+               'No Left Turn_M', 'No Right Turn_M', 'No Stopping_M', 
+               'No U_Turn Right_M', 'No U-Turn Left_M'],
+    "comment":['Longitudinal Crack', 'Raveling Crack', 'Alligator Crack', 'Transverse Crack', 'Patching', 'Pothole']
 }
